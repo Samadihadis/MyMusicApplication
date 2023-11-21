b9d165cfd72794c3a30a34ed5a3b9f09510b924f
h.samadi <samadi.hadis@gmail.com>
2023-11-21 09:53:19 +0330

    Fix problems on progress bar

--- app/src/main/java/com/hadis/mymusicapplication/currentPlayingFragment.kt
+++ app/src/main/java/com/hadis/mymusicapplication/currentPlayingFragment.kt
@@ -1,17 +1,23 @@
 package com.hadis.mymusicapplication11
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.cleveroad.play_widget.PlayLayout
 import com.hadis.mymusicapplication.currentMusic
 import com.hadis.mymusicapplication.databinding.FragmentCurrentPlayingBinding
 import com.hadis.mymusicapplication.playMusic
 import com.hadis.mymusicapplication.playerList
+import kotlinx.coroutines.launch
+import java.util.Timer
+import java.util.TimerTask
+
 
 class currentPlayingFragment : Fragment() {
+    var timer: Timer? = null
     private lateinit var playLayout: PlayLayout
     private var binding: FragmentCurrentPlayingBinding? = null
 
@@ -27,9 +33,10 @@ class currentPlayingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         playLayout = binding!!.playLayout
         playMusic(requireContext(), currentMusic!!.filePath)
+        startTimer()
         playLayout.startRevealAnimation()
         playLayout.setImageURI(currentMusic!!.coverArtUri)
-        playLayout.setOnButtonsClickListener(object : PlayLayout.OnButtonsClickListener{
+        playLayout.setOnButtonsClickListener(object : PlayLayout.OnButtonsClickListener {
             override fun onShuffleClicked() {
             }
 
@@ -43,26 +50,43 @@ class currentPlayingFragment : Fragment() {
             }
 
             override fun onPlayButtonClicked() {
-                if (playLayout.isOpen){
+                if (playLayout.isOpen) {
                     playLayout.startDismissAnimation()
                     playerList.last().pause()
-                }
-                else{
+                } else {
                     playLayout.startRevealAnimation()
                     playerList.last().play()
                 }
             }
         })
-        playLayout.setOnProgressChangedListener(object  : PlayLayout.OnProgressChangedListener{
+        playLayout.setOnProgressChangedListener(object : PlayLayout.OnProgressChangedListener {
             override fun onPreSetProgress() {
-
+                cancelTimer()
             }
 
             override fun onProgressChanged(progress: Float) {
                 playerList.last().seekTo((playerList.last().duration * progress).toLong())
+                startTimer()
             }
 
         })
+    }
+
+    private fun startTimer() {
+        timer = Timer()
+        timer!!.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                lifecycleScope.launch {
+                    playLayout.setProgress(playerList.last().currentPosition.toFloat() / playerList.last().duration)
+                }
+            }
+        }, 0, 1000)
+    }
+
+    private fun cancelTimer() {
+        if (timer == null) return
+        timer!!.cancel()
+        timer = null
     }
 
     override fun onDestroyView() {
